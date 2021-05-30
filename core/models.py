fcbb193281f91fd9c868e906a319978f1eec2d57
mohammad-ss <mohammad.azimi.1996@gmail.com>
2021-05-30 12:35:17 +0430

    core models created

--- core/models.py
+++ core/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Permision(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Position(models.Model):
+    name = models.CharField(max_length=100)
+    parent = models.ForeignKey('self' , on_delete=models.CASCADE , blank=True , null=True)
+
+    def __str__(self):
+        return self.name
+
+class Employee(models.Model):
+    user = models.OneToOneField(User , on_delete=models.CASCADE , related_name="employee")
+    department = models.CharField(max_length=150)
+    national_id = models.CharField(max_length=10)
+    birth_date = models.DateField(blank=True)
+    phone_number = models.CharField(max_length=11)
+    position = models.ForeignKey(Position , models.CASCADE , related_name="employees")
+    permission = models.ManyToManyField(Permision,related_name="employees")
+
+    def __str__(self):
+        return f'{self.user.username} ( {self.department} ) '
