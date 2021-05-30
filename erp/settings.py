977318b5133bb54db9265f567fcb4785f7ee7dcc
mohammad-ss <mohammad.azimi.1996@gmail.com>
2021-05-30 12:35:42 +0430

    core app added to settings

--- erp/settings.py
+++ erp/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
 #     local apps :
-    'cartable.apps.CartableConfig'
+    'cartable.apps.CartableConfig',
+    'core.apps.CoreConfig'
 ]
 
 MIDDLEWARE = [
